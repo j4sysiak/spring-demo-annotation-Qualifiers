467121d28c14b5c9e59c364500f527bd0d993846
Jacek <Jacek@BERLIN>
2020-02-22 01:39:26 +0100

    xx

--- spring-demo-annotation-Qualifiers/src/pl/jaceksysiak/springdemo/TennisCoach.java
+++ spring-demo-annotation-Qualifiers/src/pl/jaceksysiak/springdemo/TennisCoach.java
@@ -1,41 +1,38 @@
 package pl.jaceksysiak.springdemo;
 
-import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
-	//injecting dependency by a field
 	@Autowired
+	//@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
-
-	//injecting dependency by constructor (must have @Autowired annotation)
-//	@Autowired
-//	public TennisCoach(FortuneService theFortuneService) {
-//		fortuneService = theFortuneService;
-//	}
 	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
-	
-	
-	//injecting dependency by a setter method (must have @Autowired annotation)
-//	@Autowired
-//	public void setFortuneService(FortuneService theFortuneService) {
-//		System.out.println(">> TennisCoach: inside setFortuneService() method");
-//		this.fortuneService = theFortuneService;
-//	}
-	
-//	//injecting dependency by a any method (must have @Autowired annotation)
-//	@Autowired
-//    public void doSomeCrazyStuff(FortuneService theFortuneService) {
-//		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-//		this.fortuneService = theFortuneService;
-//	}
 
+	// define a setter method
+	/*
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		this.fortuneService = theFortuneService;
+	}
+	*/
+	
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
